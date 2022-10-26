bd3cd82a1970adc2ee854b22de710b2cf44f50f1
fluorjo <fluorjo@naver.com>
2022-10-26 20:56:04 +0900

    221025wetube_5.9.mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,38 @@
-const fakeUser = {
-    username:"j",
-    loggedIn:false,
+//const fakeUser = {username:"j",loggedIn:false,};
+
+
+export const  trending = (req,res)=>{
+    const videos = [
+        {
+            title:"First video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago",
+            views:59,
+            id:1,
+        },
+        {
+            title:"Second video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago",
+            views:59,
+            id:1,
+        },
+        {
+            title:"Third video",
+            rating:5,
+            comments:2,
+            createdAt:"2 minutes ago",
+            views:59,
+            id:1,
+        },
+    ];
+    
+    
+    return res.render("home",{pageTitle:"Home",videos});
+
 };
-
-
-export const  trending = (req,res)=>res.render("home",{pageTitle:"Home",fakeUser:fakeUser});
 export const  see = (req,res)=>res.render("watch");
 export const  edit = (req,res)=>res.render("edit");
 export const  search = (req,res)=>res.send("search");
