5b5ff692c48d892b5c923da5ce8b40e2ec4864c4
fluorjo <fluorjo@naver.com>
2022-10-27 02:59:54 +0900

    221027wetube_6.1.array Database2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,44 +1,46 @@
 //const fakeUser = {username:"j",loggedIn:false,};
+let videos = [
+    {
+        title:"First video",
+        rating:5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:1,
+        id:1,
+    },
+    {
+        title:"Second video",
+        rating:5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:2,
+    },
+    {
+        title:"Third video",
+        rating:5,
+        comments:2,
+        createdAt:"2 minutes ago",
+        views:59,
+        id:3,
+    },
+];
+
 
 
 export const  trending = (req,res)=>{
-    const videos = [
-        {
-            title:"First video",
-            rating:5,
-            comments:2,
-            createdAt:"2 minutes ago",
-            views:59,
-            id:1,
-        },
-        {
-            title:"Second video",
-            rating:5,
-            comments:2,
-            createdAt:"2 minutes ago",
-            views:59,
-            id:1,
-        },
-        {
-            title:"Third video",
-            rating:5,
-            comments:2,
-            createdAt:"2 minutes ago",
-            views:59,
-            id:1,
-        },
-    ];
-    
-    
     return res.render("home",{pageTitle:"Home",videos});
-
 };
-export const  see = (req,res)=>res.render("watch");
+export const  watch = (req,res)=>{
+    const { id } = req.params;
+    //const id = req.params.id;
+    //params는 video router에서 받아옴
+    const video = videos[id-1];
+    
+    return res.render("watch",{pageTitle: `Watching ${video.title}`, video});
+};
 export const  edit = (req,res)=>res.render("edit");
 export const  search = (req,res)=>res.send("search");
 export const  upload = (req,res)=>res.send("upload");
-export const  deleteVideo = (req,res)=>{
-    console.log(req.params);
-    return res.send("deleteVideo");
-}
+export const  deleteVideo = (req,res)=>res.send("deleteVideo");
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {see, edit,upload,deleteVideo} from "../controllers/videoController";
+import {watch, edit,upload,deleteVideo} from "../controllers/videoController";
 const  videoRouter=express.Router() ;
 
 videoRouter.get("/upload",upload);
 //순서 주의. upload가 id보다 먼저 와야 함. 
 //안 그러면 express가 upload라는 텍스트 자체를 id라고 인식해버림.
-videoRouter.get("/:id(\\d+)",see);
+videoRouter.get("/:id(\\d+)",watch);
 videoRouter.get("/:id(\\d+)/edit",edit);
 videoRouter.get("/:id(\\d+)/delete",deleteVideo);
 export default videoRouter;
