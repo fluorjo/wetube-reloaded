81692812159fc9b1de38f54298db6866c397e17d
fluorjo <fluorjo@naver.com>
2022-10-29 09:32:16 +0900

    221028wetube_6.18. more schema

--- src/models/video.js
+++ src/models/video.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: {type:String, required:true},
+    title: {type:String, required:true, trim:true,maxLength:80},
     //{type:String}이라고 해도 됨.
-    description: {type:String, required:true},
+    description: {type:String, required:true, trim:true,minLength:20},
     createdAt: {type:Date, required:true,default:Date.now},
-    hashtags:[{ type: String}],
+    hashtags:[{ type: String, trim:true, trim:true, trim:true, trim:true, trim:true, trim:true, trim:true}],
     //배열로 만든 것.
     meta:{
         views:{type:Number, default:0, required:true},
