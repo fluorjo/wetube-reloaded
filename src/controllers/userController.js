0fca690364c3f6bde001833cd7156aa3d9534901
fluorjo <fluorjo@naver.com>
2022-11-05 23:28:14 +0900

    221105 8.3.edit profile post2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -163,11 +163,18 @@ export const postEdit= async(req,res)=>{
     body:{name, email, username, location},
   }= req;
   //const id = req.session.user.id
-
-  await User.findByIdAndUpdate(_id,{
-    name,email, username, location,
-  });
-  return res.render("edit-profile",{pageTitle:"Edit Profile"});
-};
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
+  };
 
 export const  see = (req,res)=>res.send("see");
