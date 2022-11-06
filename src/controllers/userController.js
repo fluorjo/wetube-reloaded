eb1406a95fcdfc121d4d11a8b11dc708d93f24f4
fluorjo <fluorjo@naver.com>
2022-11-07 01:42:23 +0900

    221107 10.style end

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/User";
-import Video from "../models/Video";
+
 import bcrypt from "bcrypt";
 //import { application } from "express";
 import fetch from "node-fetch";
@@ -212,9 +212,15 @@ export const postChangePassword = async(req,res)=>{
   //return res.redirect("/");
 };
 
-export const  see = async(req,res)=>{
+export const see = async(req,res)=>{
   const {id} = req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
 
   if(!user){
     return res.status(404).render("404",{pageTitle:"User not found"
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const  home = async(req,res)=>{
-        const  videos = await Video.find({}).sort({createdAt:"desc"});
+        const videos = await Video.find({})
+            .sort({ createdAt: "desc" })
+            .populate("owner");
         return res.render("home",{pageTitle:"Home", videos});
 };
 export const  watch = async(req,res)=>{
@@ -109,7 +111,7 @@ export const search = async(req,res)=>{
                 $regex: new RegExp(keyword, "i"),
                 //"i" 대소문자 구분 없애기
             }
-        });
+        }).populate("owner");
     } 
     return res.render("search",{pageTitle:"Search", videos});
 };
