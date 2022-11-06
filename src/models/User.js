e85cc48acdd5a285b87fe5f75464142d3cd0765c
fluorjo <fluorjo@naver.com>
2022-11-06 09:06:29 +0900

    221106 8.13.user's videos

--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,9 @@ const userSchema = new mongoose.Schema({
     name: {type:String, required:true},
     password: { type: String },
     location: String,
+    videos:[{
+        type:mongoose.Schema.Types.ObjectId,ref:"Video"
+    }],
 
 });
 
