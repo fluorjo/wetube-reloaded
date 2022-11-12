3cac0bf73654e8a7c39e67e22671f834e9645e33
fluorjo <fluorjo@naver.com>
2022-11-13 05:55:54 +0900

    221113 production environment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -167,10 +167,11 @@ export const postEdit= async(req,res)=>{
   }= req;
   //console.log("path:", path);
   //const id = req.session.user.id
+  const isHeroku=process.env.NODE_ENV==="production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.location :avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -10,11 +10,33 @@ const s3 = new S3Client({
     },
     });
 
-const multerUploader = multerS3({
+const isHeroku=process.env.NODE_ENV==="production";
+
+const s3ImageUploader = multerS3({
+    s3: s3,
+    bucket: "nottube",
+    acl: "public-read",
+    // bucket 안에 folder 속에 file 분류하기
+    key: function (request, file, ab_callback) {
+    const newFileName = Date.now() + "-" + file.originalname;
+    const fullPath = "images/" + newFileName;
+    ab_callback(null, fullPath);
+    },
+    });
+
+
+const s3VideoUploader = multerS3({
     s3:s3,
     bucket:'nottube',
     acl:"public-read",
+
+    key: function (request, file, ab_callback) {
+        const newFileName = Date.now() + "-" + file.originalname;
+        const fullPath = "videos/" + newFileName;
+        ab_callback(null, fullPath);
+        },
 });
+
 
 
 
@@ -23,6 +45,7 @@ export const localsMiddleware = (req,res,next)=>{
     res.locals.loggedIn=Boolean(req.session.loggedIn);
     res.locals.siteName="Wetube";
     res.locals.loggedInUser=req.session.user ||{};
+    res.locals.isHeroku = isHeroku;
     next();
 };
 
@@ -49,12 +72,12 @@ export const avatarUpload= multer({
     limits:{
     fileSize: 3000000,
 },
-storage:multerUploader,
+storage: isHeroku ? s3ImageUploader : undefined,
 });
 export const videoUpload= multer({
     dest:"uploads/videos/", 
     limits:{
     fileSize: 10000000,
 },
-storage:multerUploader,
+storage: isHeroku ? s3VideoUploader : undefined,
 });
