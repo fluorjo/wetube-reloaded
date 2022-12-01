17f51cdced4006d39b2b2798e5be51c1a8dd60d1
fluorjo <fluorjo@naver.com>
2022-12-01 18:42:31 +0900

    새창에스샷열기성공.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -266,8 +266,12 @@ const capture =() =>{
     
     screenShotAnchor.href = dataURL;
     console.log(screenShotAnchor.href);
-    screenShotAnchor.download = "capture.png";
-    screenShotAnchor.click();
+
+    let win = window.open();
+    win.document.write('<iframe src="' + dataURL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+
+
+    // screenShotAnchor.click();
 };
 
 screenShotAnchor.addEventListener("click", capture);
